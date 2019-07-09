b54f3c5d9cd3acc0cf3c4c15591a7dc58ba727bc
alamistiqha <alamistiqha@gmail.com>
2019-07-09 12:22:35 +0800

    alam File 2

---
+++ File2.php
@@ -0,0 +1,33 @@
+<?php
+$pilihan = $_POST['metode'];
+if ($pilihan == "fibbonaci"){
+    $nama = $_POST['username'];
+    echo 'nama anda : '.$nama.'<br>';
+    $a = 0;
+    $b = 1;
+    $c = 0;
+
+    while($c<10){
+        echo ' '.$c;
+        $a = $b;
+        $b = $c;
+        $c = $a + $b;
+    }
+    }else if ($pilihan == 'Ganjil'){
+    $nama = $_POST['username'];
+    echo 'nama anda : '.$nama.'<br>';
+    for($a = 1; $a <= 10; $a++){
+        if ($a % 2 != 0){
+            echo' '.$a;
+        }
+    }
+}else if ($pilihan == 'Genap'){
+    $nama = $_POST['username'];
+    echo 'nama anda : '.$nama.'<br>';
+    for($a = 1; $a <= 10; $a++){
+        if ($a % 2 == 0){
+            echo' '.$a;
+        }
+    }
+
+?>
